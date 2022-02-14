bcba9b5c3dd771bb8f03582817e3c4f27eceeb6d
Alex-Mercy <hambiru@yandex.ru>
2022-02-14 21:39:53 +0300

    Updates

---
+++ precache-manifest.4b42a7633efb441890408a54d942e293.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "46a792dfe4e572b7b7fcf0e726dc3e8d",
+    "url": "/note-keeper/index.html"
+  },
+  {
+    "revision": "f2a84a7fc147757bbb96",
+    "url": "/note-keeper/static/js/2.7f818d8f.chunk.js"
+  },
+  {
+    "revision": "e68b61f5b689d6ebd216",
+    "url": "/note-keeper/static/js/main.776d059f.chunk.js"
+  },
+  {
+    "revision": "5c09552dff8737d2174e",
+    "url": "/note-keeper/static/js/runtime-main.4bdd423f.js"
+  }
+]);
